976b9ce4d0d2491587376504fd54fc732adca960
Gyandeep <gyanadeeps@gmail.com>
2022-05-30 23:03:30 +0530

    Login test case v1

--- WEB_UI_Module/src/main/java/pageobjects/pages/SpotOnSignUp.java
+++ WEB_UI_Module/src/main/java/pageobjects/pages/SpotOnSignUp.java
@@ -1,6 +1,14 @@
 package pageobjects.pages;
 
-public interface SpotOnSignUp
-{
+import pageobjects.PageFactoryClasses.SpotOnPageFactory;
 
+public class SpotOnSignUp implements ISpotOnSignUp
+
+{
+    @Override
+    public ISpotOnSignUp verifyButton()
+    {
+        System.out.println("SignUp Page");
+        return SpotOnPageFactory.getSignUpPage();
+    }
 }
